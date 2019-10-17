00088fe82588a78290040ba1a569b31863993acc
Yamada <pan.kong@bibsolution.com>
2019-10-17 10:10:00 +0800

    error fix & formatting

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,31 +1,33 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
+document.addEventListener('DOMContentLoaded', function () {
+    const elems = document.querySelectorAll('.fixed-action-btn');
+    const instances = M.FloatingActionButton.init(elems, {
         direction: 'left',
         hoverEnabled: false
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-    // var instance = M.Sidenav.getInstance($('.sidenav'));
+    // const instance = M.Sidenav.getInstance($('.sidenav'));
     // instance.open();
 });
 
-function scrollToBot(){
-    var dialogWrapper = $('#card-content');
-    var height = dialogWrapper[0].scrollHeight;
-    dialogWrapper.scrollTop(height);
+function scrollToBot() {
+    const dialogWrapper = $('#card-content');
+    if (dialogWrapper[0]) {
+        const height = dialogWrapper[0].scrollHeight;
+        dialogWrapper.scrollTop(height);
+    }
 }
 
-(function($){
-    $(window).on("load",function(){
-        // var Scrollbar = window.Scrollbar;
+(function ($) {
+    $(window).on("load", function () {
+        // const Scrollbar = window.Scrollbar;
         // Scrollbar.initAll();
         // Scrollbar.init(document.querySelector('#card-content'));
-    
-        $("#logout-btn").click(function(e){
+
+        $("#logout-btn").click(function (e) {
             e.preventDefault();
             alert("logout")
         });
